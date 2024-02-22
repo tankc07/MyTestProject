bac55cea1743b933a083a7544df236efb16ef3c8
fs <tankc07@163.com>
2024-02-22 15:55:53 +0800

    修复: 数据库修改不全的问题, IP172.16.1.15, 账号dsby, 密码dsby, 端口3341, 修改4个位置.
    数据库Ip: 172.16.7.45 -> 172.16.1.15
    用户名: sa -> dsby
    密码: SqlA123b456c789. ->dsby
    端口: 1433 -> 3341
    涉及到的位置: api测试/Form1.cs, BLL/Bll.cs, BLL/FileHandle.cs, TestControl/Program.cs

--- BLL/FileHandle.cs
+++ BLL/FileHandle.cs
@@ -9,7 +9,7 @@ namespace BLL
 {
 	public class FileHandle
 	{
-		static YJT.DataBase.DbHelper _ecSqlServer = new YJT.DataBase.DbHelperSqlServer("172.16.7.45", "YanduECommerceAutomaticPrinting", "sa", "SqlA123b456c789.", "1433");
+		static YJT.DataBase.DbHelper _ecSqlServer = new YJT.DataBase.DbHelperSqlServer("172.16.1.15", "YanduECommerceAutomaticPrinting", "dsby", "dsby", "3341");
 		public enum EnumFtpCompany
 		{
 			汇达ErpFtp,
