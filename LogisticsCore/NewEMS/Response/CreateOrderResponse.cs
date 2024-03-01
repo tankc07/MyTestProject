69e031b31c845ae21925e9ced634b690e1584879
fs <tankc07@163.com>
2024-03-02 04:51:15 +0800

    修改: 修改请求响应实体类, 解决响应报文中包含字符串格式的Json字符串, 无法正常反序列化.

--- LogisticsCore/NewEMS/Response/CreateOrderResponse.cs
+++ LogisticsCore/NewEMS/Response/CreateOrderResponse.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace LogisticsCore.NewEMS.Response
 {
     public class CreateOrderResponse:NewEmsResponseBase
     {
         #pragma warning disable IDE1006 // 命名样式
-        public CreateOrderResponseBody retBody { get; set; }
+        public CreateOrderResponseBody retBodyObj
+        {
+            get
+            {
+                if (retBody == null) return null;
+                try
+                {
+                    return JsonConvert.DeserializeObject<CreateOrderResponseBody>(retBody);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
         #pragma warning restore IDE1006 // 命名样式
     }
 }
