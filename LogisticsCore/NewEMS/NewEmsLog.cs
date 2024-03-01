62df835faff68b7c622f873df644d3fc8b95d877
fs <tankc07@163.com>
2024-03-02 04:46:59 +0800

    新增: 增加NewEmsLog实体类, 测试用.

---
+++ LogisticsCore/NewEMS/NewEmsLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticsCore.NewEMS
+{
+    public class NewEmsLog
+    {
+        public string datetime { get; set; }
+        public string responseContent { get; set; }
+        public string contentToDynamicObj { get; set; }
+        public string dynamicObjToJsonText { get; set; }
+
+        public string convertDynamicJsonToCreateOrderResponse { get; set; }
+        public string responseError { get; set; }
+        public string contentNull { get; set; }
+        public string catchErr { get; set; }
+    }
+}
