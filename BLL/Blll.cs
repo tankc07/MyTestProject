591f117494b17b07f37c07f17d50db7409c8a7dd
fs <tankc07@163.com>
2024-03-19 17:12:48 +0800

    修改: 重新修改为将3公斤已下的6个省份快递选择为新邮政Ems

--- BLL/Blll.cs
+++ BLL/Blll.cs
@@ -1062,16 +1062,16 @@ where
                             //开始计算
                             if (order.Weight <= 3.0)
                             {
-                                order.Logic = Settings.Setings.EnumLogicType.申通快递;
-                                //if (YJT.Text.Verification.IsLikeIn(order.PROVINCENAME, new List<string>() { "山西" }, true))
-                                //{
-                                //    //Modify: 修改时间: 2024-02-29 By:Ly 修改内容: 重量小于3公斤的订单,从默认使用申通快递 => 新邮政Ems
-                                //    order.Logic = Settings.Setings.EnumLogicType.新邮政Ems;
-                                //}
-                                //else
-                                //{
-                                //    order.Logic = Settings.Setings.EnumLogicType.申通快递;
-                                //}
+                                //order.Logic = Settings.Setings.EnumLogicType.申通快递;
+                                if (YJT.Text.Verification.IsLikeIn(order.PROVINCENAME, new List<string>() { "北京", "天津", "河北", "河南", "山东", "山西" }, true))
+                                {
+                                    //Modify: 修改时间: 2024-02-29 By:Ly 修改内容: 重量小于3公斤的订单,从默认使用申通快递 => 新邮政Ems
+                                    order.Logic = Settings.Setings.EnumLogicType.新邮政Ems;
+                                }
+                                else
+                                {
+                                    order.Logic = Settings.Setings.EnumLogicType.申通快递;
+                                }
                             }
                             else
                             {
@@ -7481,16 +7481,16 @@ WHERE Bid={order.Bid}
                                 //开始计算
                                 if (order.Weight <= 3.0)
                                 {
-                                    order.Logic = Settings.Setings.EnumLogicType.申通快递;
-                                    //if (YJT.Text.Verification.IsLikeIn(order.PROVINCENAME, new List<string>() { "山西" }, true))
-                                    //{
-                                    //    //Modify: 修改时间: 2024-02-29 By:Ly 修改内容: 重量小于3公斤的订单,从默认使用申通快递 => 新邮政Ems
-                                    //    order.Logic = Settings.Setings.EnumLogicType.新邮政Ems;
-                                    //}
-                                    //else
-                                    //{
-                                    //    order.Logic = Settings.Setings.EnumLogicType.申通快递;
-                                    //}
+                                    //order.Logic = Settings.Setings.EnumLogicType.申通快递;
+                                    if (YJT.Text.Verification.IsLikeIn(order.PROVINCENAME, new List<string>() { "北京", "天津", "河北", "河南", "山东", "山西" }, true))
+                                    {
+                                        //Modify: 修改时间: 2024-02-29 By:Ly 修改内容: 重量小于3公斤的订单,从默认使用申通快递 => 新邮政Ems
+                                        order.Logic = Settings.Setings.EnumLogicType.新邮政Ems;
+                                    }
+                                    else
+                                    {
+                                        order.Logic = Settings.Setings.EnumLogicType.申通快递;
+                                    }
                                 }
                                 else
                                 {
